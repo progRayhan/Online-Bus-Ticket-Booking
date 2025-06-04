2cd3c16c6a5fb6114ed08bba2df44fe4f4a98502
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2025-06-04 20:36:02 +0600

    feat: sqlite database setup

---
+++ database.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+def database_of_bus_ticket():
+    """
+    Establishes a connection to the 'bus_ticket.db' SQLite database and
+    returns both the connection and cursor objects.
+    """
+
+    db_conn = sqlite3.connect("bus_ticket.db")
+    cursor = db_conn.cursor()
+
+    return db_conn, cursor
+
+
+def execute_sql(sql_query):
+    """
+    Executes a given SQL query using the bus_ticket.db connection.
+    """
+
+    db_conn, cursor = database_of_bus_ticket()
+
+    cursor.execute(sql_query)
+
+    db_conn.commit()
+    db_conn.close()
