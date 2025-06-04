223d6f8b64eb3a8575d5483042129353fca96659
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2025-06-04 21:18:55 +0600

    feat: script for bus data added

--- database.py
+++ database.py
@@ -12,14 +12,19 @@ def database_of_bus_ticket():
     return db_conn, cursor
 
 
-def execute_sql(sql_query):
+def execute_sql(sql_query, params=None):
     """
     Executes a given SQL query using the bus_ticket.db connection.
     """
 
     try:
         db_conn, cursor = database_of_bus_ticket()
-        cursor.execute(sql_query)
+
+        if params:
+            cursor.executemany(sql_query, params)
+        else:
+            cursor.execute(sql_query)
+
         db_conn.commit()
         db_conn.close()
         return "Query executed successfully"
